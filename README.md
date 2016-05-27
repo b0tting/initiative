822558253d5dbc4047a07e415e62b8fd1140c58b
root <root@kraken.woodyways>
2016-05-27 12:45:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Initiative!
+We like playing D20 based games - pathfinder, Dungeons and dragons 3rd edition, D&D Next, etcetera. A lot of time goes into combat and administrating combat. Who'se turn is it? But shouldn't Alice have gone first? 
+
+These discussions take the speed out of the game, a problem this page tries to solve by delegating to players. All players can open the initiative app and allow the DM to delegate the calling of rounds and round based effect to the players. 
+
+An example of play: 
+- DM: "You step on a branch. The loud crack does not seem to surprise the orc, who had already nocked an arrow. He turns around to face you."
+- DM: "Everybody, add your initiative. Alice, can you call off the rounds for me? Please add the orc with a +4 initative bonus. Also add a "surprise" effect that lasts three rounds and tell me when it hits zero"
+- Alice adds her initiative, one for the orc and also adds an effect called "surprise" that last three rounds. 
+- Alice: "Okay, Bob, I see you go first"
+- Bob: "I delay until after the orc shoots."
+- Bob drags his initiative down. He punches "Next"
+- Bob: "You're up now, Alice"
+
+# Loose rules
+The app ís run on a small webserver that send updates to all connected browsers. Every player can add new entries, roll turns and add "effects" such as spells or whatever the DM wants. The app does not enforce rules to allow players to bend and fudge the story in whatever way suits them. 
+
+- add your initiative and either roll a die or have the app roll it for you
+- remembers the player in the current browser for easier adding in a next combat
+- drag character up or down for the "delay" action
+- swipe entries left or right to delete them
+- click the "round" button on top to only show the initiative that is currently up
+
+# Installation
+This web server is dependent on the flask-socketio and gevent libraries. Assuming you have installed pip:
+```sh
+pip install flask-socketio gevent gevent-websocket
+```
+Note that the gevent installation will fail on Windows. The error message will direct you to install a VC++ package, which will solve this problem. 
+
+Run the script by starting with python:
+```sh
+python present.py
+````
+# Webserver
+By default, the webserver starts on port 85. Change this in the last line of present.py. This application is based on websockets, which will give you some trouble if you want to proxy from an apache machine. In that case, assuming you already have an Apache 2.4 and proxy_wstunnel, you can use the following configuration: 
+```
+<VirtualHost *:80>
+  ProxyPreserveHost On
+  RewriteEngine on
+  ProxyRequests Off
+  
+  RewriteCond %{QUERY_STRING} transport=polling
+  ### Use the IP running the present.py webserver
+  RewriteRule /(.*)$ http://127.0.0.1:85/$1 [P]
+
+  ProxyPass /socket.io ws://127.0.0.1:85/socket.io/
+  ProxyPass / http://127.0.0.1:85/
+  ProxyPassReverse / http://127.0.0.1:85/
+</VirtualHost>
+
+```
---
+++ present.py
@@ -0,0 +1,204 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+## pip install flask-socketio gevent gevent-websocket
+
+class Initiative:
+    def __init__(self, name, bonus, roll):
+        self.name = name
+        self.bonus = int(bonus)
+        self.roll = int(roll)
+        self.turn = False
+        self.total = self.bonus + self.roll
+
+    def is_earlier_than(self, other):
+        if self.total != other.total:
+            return self.total > other.total
+        else:
+            return self.bonus > other.bonus
+
+    def set_turn(self, turn):
+        self.turn = turn
+
+    def get_as_dict(self):
+        return {"name":self.name,"total":self.total,"turn":self.turn}
+
+
+class Effect:
+    def __init__(self, name, duration):
+        self.name = name;
+        self.duration = duration
+
+
+class GameState:
+    effects = []
+    initiatives = []
+    round = 0
+
+    def add_effect(self, name, duration):
+        self.effects.append(Effect(name, duration))
+
+    def add_initiative(self, new_init):
+        insert_at = 0
+        for init in self.initiatives:
+            if new_init.is_earlier_than(init):
+                break
+            else:
+                insert_at += 1
+        self.initiatives.insert(insert_at, new_init)
+
+    def next_round(self, rollback=False):
+        for eff in self.effects:
+
+            eff.duration = eff.duration + (1 if rollback else -1)
+
+    def clear(self):
+        self.effects[:] = []
+        self.initiatives[:] = []
+        self.round = 0
+
+    ## Vals spsler! Als er meerdere "bless" effecten zijn wordt nu altijd de eerste verwijderd!
+    def remove_effect(self, name):
+        i = 0
+        while i < len(self.effects):
+            if self.effects[i].name == name:
+                self.effects.pop(i)
+                break
+            i += 1
+
+    def remove_initiative(self, index):
+        init = self.initiatives.pop(index)
+        if(init.turn):
+            ## Net 1 gepopt
+            if(index <= len(self.initiatives)):
+                self.set_current(index)
+            else:
+                self.set_current(0)
+
+    def move_initiative(self, old_index, new_index):
+        init = self.initiatives[old_index]
+        self.initiatives.insert(new_index, self.initiatives.pop(old_index))
+        if init.turn:
+            self.set_current(old_index)
+
+    def next_initiative(self):
+        if len(self.initiatives) > 0:
+            current_index = self.get_current()
+            if current_index + 1 < len(self.initiatives):
+                new_index = current_index + 1
+            else:
+                new_index = 0
+                self.round += 1
+                for eff in self.effects:
+                    eff.duration -= 1
+                trigger_effects_update_message()
+            self.set_current(new_index)
+
+    def rollback_initiative(self):
+        if len(self.initiatives) > 0:
+            current_index = self.get_current()
+            if current_index - 1 < 0:
+                if self.round > 0:
+                    new_index = len(self.initiatives) - 1
+                    self.round -= 1
+                    for eff in self.effects:
+                        eff.duration += 1
+                    trigger_effects_update_message()
+                    self.set_current(new_index)
+            else:
+                new_index = current_index - 1
+                self.set_current(new_index)
+
+    def get_effects_dict(self):
+        return [{"name": eff.name, "rounds": eff.duration if eff.duration > 0 else 0} for eff in self.effects]
+
+    def get_initiatives_dict(self):
+        return [init.get_as_dict() for init in self.initiatives]
+
+    def set_current(self,to_index):
+        i = 0
+        while i < len(self.initiatives):
+            self.initiatives[i].set_turn(i == to_index)
+            i += 1
+
+    def get_current(self):
+        i = 0
+        while i < len(self.initiatives):
+            if self.initiatives[i].turn:
+                break
+            i += 1
+        return i
+
+@socketio.on('deleteeffect', namespace='/state')
+def delete_effect(data):
+    effects.remove_effect(data["name"])
+    trigger_effects_update_message()
+
+@socketio.on('addeffect', namespace='/state')
+def add_effect(data):
+    effects.add_effect(data["name"], int(data["duration"]))
+    trigger_effects_update_message()
+
+@socketio.on('move', namespace='/state')
+def move_initative(data):
+    effects.move_initiative(int(data["old_index"]), int(data["new_index"]))
+    trigger_initiative_update_message()
+
+@socketio.on('next', namespace='/state')
+def next_initiative():
+    effects.next_initiative()
+    trigger_initiative_update_message()
+
+@socketio.on('rollback_next', namespace='/state')
+def former_initiative():
+    effects.rollback_initiative()
+    trigger_initiative_update_message()
+
+@socketio.on('clear', namespace='/state')
+def clear_initiatives():
+    effects.clear()
+    trigger_initiative_update_message()
+    trigger_effects_update_message()
+
+@socketio.on('delete', namespace='/state')
+def delete_initiative(data):
+    effects.remove_initiative(int(data["index"]))
+    trigger_initiative_update_message()
+
+@socketio.on('getstate', namespace='/state')
+def test_message():
+    emit('newstate', get_initiative_state())
+    emit('neweffects', get_effects_state())
+
+def trigger_initiative_update_message():
+    emit('newstate', get_initiative_state(), broadcast=True, namespace="/state")
+
+def trigger_effects_update_message():
+    emit('neweffects', get_effects_state(), broadcast=True, namespace="/state")
+
+@app.route('/')
+def versus_the_world():
+    return render_template('initiative.html')
+
+@socketio.on('add', namespace='/state')
+def add_initiative(data):
+    effects.add_initiative(Initiative(data["name"], int(data["bonus"]), int(data["roll"])))
+    trigger_initiative_update_message()
+
+def get_initiative_state():
+    return {"initiatives": effects.get_initiatives_dict(), "round": effects.round}
+
+def get_effects_state():
+    return {"effects":effects.get_effects_dict()}
+
+@app.route('/present')
+def initiative():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    effects = GameState()
+    socketio.run(app, host='0.0.0.0',port=85,debug=False)
+
